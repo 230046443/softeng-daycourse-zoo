db7fb69e8f97640c770f15bf157af37ed4ab45b8
Abdullah Saleem <230046443@aston.ac.uk>
2026-01-15 14:23:35 +0000

    Lion.py added to src/zoo

---
+++ src/zoo/lion.py
@@ -0,0 +1,15 @@
+
+# lion.py
+
+from .animal import Animal
+
+
+class Lion(Animal):
+    def __init__(self, name="Asad"):
+        super().__init__(name, species="Lion")
+
+    def sound(self):
+        return "roars"
+
+    def action(self):
+        return "King of the jungle."
